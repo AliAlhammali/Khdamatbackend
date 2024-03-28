e0acb7b7d3bf50a1cb388f77084a9e1c1b4bf6ae
Ahmed Ebead <ahmed.ebead@munjz.com>
2024-03-28 21:05:06 +0200

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         \App\Models\User::factory(10)->create();
-
-         \App\Models\User::factory()->create([
-             'name' => 'Admin User',
-             'email' => 'admin@khadamat-teck.test',
-             'password'=>bcrypt(123456)
-         ]);
+//         \App\Models\User::factory(10)->create();
+//
+//         \App\Models\User::factory()->create([
+//             'name' => 'Admin User',
+//             'email' => 'admin@khadamat-teck.test',
+//             'password'=>bcrypt(123456)
+//         ]);
     }
 }
