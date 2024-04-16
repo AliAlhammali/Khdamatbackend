e2cb139e3d354629604125f435dc8aab09dd8b17
Ahmed Ebead <ahmed.ebead@munjz.com>
2024-04-16 21:13:05 +0200

    new changes

---
+++ database/migrations/2024_04_16_191106_order_tables.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('pick_up_type')->default('delivered')->comment('delivered,from_branch');
+            $table->foreignIdFor(\App\KhadamatTeck\Admin\MerchantBranches\Models\MerchantBranch::class)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
