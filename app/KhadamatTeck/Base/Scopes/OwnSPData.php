da89b1542b8bbace660e51215c8208b20bc9eb80
Ahmed Ebead <ahmed.ebead@munjz.com>
2024-05-06 13:50:35 +0300

    add filters

--- app/KhadamatTeck/Base/Scopes/OwnSPData.php
+++ app/KhadamatTeck/Base/Scopes/OwnSPData.php
@@ -14,13 +14,12 @@ class OwnSPData implements Scope
     public function apply(Builder $builder, Model $model): void
     {
 
-        if (SPAuth()->user()) {
-            $role = SPAuth()->user()->role;
-
-            if ($role == 'Admin')
+//        if (SPAuth()->check()) {
+//            $role = SPAuth()->user()->role;
+//            if ($role == 'Admin')
                 $builder->where('service_provider_id', SPAuth()->user()->service_provider_id);
-            else
-                $builder->where('service_provider_id', SPAuth()->user()->service_provider_id)->where('service_provider_user_id', SPAuth()->user()->id);
-        }
+//            else
+//                $builder->where('service_provider_id', SPAuth()->user()->service_provider_id)->where('service_provider_user_id', SPAuth()->user()->id);
+//        }
     }
 }
