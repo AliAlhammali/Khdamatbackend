f28269184819e37defd2fdda8876d71f0f89340e
Ahmed Ebead <ahmed.ebead@munjz.com>
2024-08-07 19:21:35 +0300

    hot fix

--- database/migrations/2024_08_07_155819_alter_order_items.php
+++ database/migrations/2024_08_07_155819_alter_order_items.php
@@ -4,19 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('order_items', function (Blueprint $table) {
-            $table->string('category_id')->nullable();
-        });
-        Schema::table('orders', function (Blueprint $table) {
-            $table->string('category_id')->nullable()->change();
-        });
+        if (!Schema::hasColumn('order_items', 'category_id'))
+            Schema::table('order_items', function (Blueprint $table) {
+                $table->string('category_id')->nullable();
+            });
+        if (Schema::hasColumn('orders', 'category_id'))
+            Schema::table('orders', function (Blueprint $table) {
+                $table->string('category_id')->nullable()->change();
+            });
     }
 
     /**
